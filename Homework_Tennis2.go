3e3362faab66983c7c8679dcaf35e0aada9a71ce
Chayakorn <oatamanoob@gmail.com>
2019-08-26 17:31:11 +0700

    commit

--- Homework_Tennis2.go
+++ Homework_Tennis2.go
@@ -18,20 +18,20 @@ type Game struct {
 }
 
 func (g Game) CurrentScore() {
-	if g.PlayerA.point < 4 && g.PlayerB.point < 4 && (g.PlayerA.point < 3 || g.PlayerB.point < 3) {
+
+	switch {
+	case g.PlayerA.point < 4 && g.PlayerB.point < 4 && (g.PlayerA.point < 3 || g.PlayerB.point < 3):
 		fmt.Printf("%s - %s\n", SCORE[g.PlayerA.point], SCORE[g.PlayerB.point])
-	} else if g.PlayerA.point == g.PlayerB.point {
+	case g.PlayerA.point == g.PlayerB.point:
 		fmt.Printf("%s\n", SCORE[DEUCE])
-	} else if g.PlayerA.point-g.PlayerB.point == 1 {
+	case g.PlayerA.point-g.PlayerB.point == 1:
 		fmt.Printf("%s %s\n", SCORE[ADV], g.PlayerA.name)
-	} else if g.PlayerB.point-g.PlayerA.point == 1 {
+	case g.PlayerB.point-g.PlayerA.point == 1:
 		fmt.Printf("%s %s\n", SCORE[ADV], g.PlayerB.name)
-	} else { // someone win
-		if g.PlayerA.point > g.PlayerB.point {
-			fmt.Printf("%s %s\n", g.PlayerA.name, SCORE[WIN])
-		} else {
-			fmt.Printf("%s %s\n", g.PlayerB.name, SCORE[WIN])
-		}
+	case g.PlayerA.point > g.PlayerB.point:
+		fmt.Printf("%s %s\n", g.PlayerA.name, SCORE[WIN])
+	default:
+		fmt.Printf("%s %s\n", g.PlayerB.name, SCORE[WIN])
 	}
 }
 
@@ -50,13 +50,5 @@ func main() {
 	g.PlayerAGetPoint()
 	g.PlayerBGetPoint()
 	g.PlayerBGetPoint()
-	g.PlayerBGetPoint()
-	g.PlayerAGetPoint()
-	g.PlayerAGetPoint()
-	g.PlayerAGetPoint()
-	g.PlayerBGetPoint()
-	g.PlayerBGetPoint()
-	g.PlayerBGetPoint()
 	g.CurrentScore()
-	// Test more 
 }
